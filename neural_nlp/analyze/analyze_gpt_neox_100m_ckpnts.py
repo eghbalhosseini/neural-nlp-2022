62d636b0f7a314abcd94e4cfb0bf9d179dd5f226
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-21 08:40:33 -0400

    adding gpt_neox models

--- neural_nlp/analyze/analyze_gpt_neox_100m_ckpnts.py
+++ neural_nlp/analyze/analyze_gpt_neox_100m_ckpnts.py
@@ -20,12 +20,9 @@ def get_args():
     return args
 
 def mock_get_args():
-    mock_args = namedtuple('debug', ['model_list', 'benchmark'])
-    new_args = mock_args(['gpt2-neox-pos_learned-1M-v2-ckpnt-1000',
-                          'gpt2-neox-pos_learned-10M-v2-ckpnt-2250',
-                          'gpt2-neox-pos_learned-100M-v2-ckpnt-14250',
-                          'gpt2-neox-pos_learned-1B-v2-ckpnt-155000'],
-                         'Fedorenko2016v3-encoding')
+    mock_args = namedtuple('debug', ['model_prefix', 'benchmark'])
+    #new_args = mock_args('gpt2-neox-pos_learned-100M-v2-ckpnt', 'Fedorenko2016v3-encoding')
+    new_args = mock_args('gpt2-neox-pos_learned-100M-v2-ckpnt', 'Pereira2018-encoding')
     return new_args
 
 debug = True
@@ -38,20 +35,18 @@ if __name__ == '__main__':
     else:
         args = get_args()
 
-    model_scores=[]
-    for model_ckpnt in args.model_list:
-        file_pattern = f"benchmark={args.benchmark},model={model_ckpnt}*"
-        print(f"finding {file_pattern} in {score_dir}")
-        model_scores.append(glob.glob(os.path.join(score_dir,file_pattern)))
+    file_pattern=f"benchmark={args.benchmark},model={args.model_prefix}*"
+    print(f"finding {file_pattern} in {score_dir}")
+    model_scores=glob.glob(os.path.join(score_dir,file_pattern))
     re_p=re.compile(f"pos_learned-\d+\w+")
-    temp = [re.findall(re_p, x[0]) for x in model_scores]
-    untrained=['untrained' in x[0] for x in model_scores]
+    temp = [re.findall(re_p, x) for x in model_scores]
+    untrained=['untrained' in x for x in model_scores]
     re_ckpnt=re.compile(f"ckpnt-\d+")
-    temp1 = [int(re.findall(re_ckpnt, x[0])[0].replace('ckpnt-','')) for x in model_scores]
-    #reindex=np.argsort(temp1)
-    #model_scores=[model_scores[x] for x in reindex]
+    temp1 = [int(re.findall(re_ckpnt, x)[0].replace('ckpnt-','')) for x in model_scores]
+    reindex=np.argsort(temp1)
+    model_scores=[model_scores[x] for x in reindex]
     #mdl_d_sz=[x[0].split('-')[-1] for x in temp]
-    ckpt_ids=(temp1)
+    ckpt_ids=np.sort(temp1)
 
     #
     #model_scores=[model_scores[idx] for idx, x in enumerate(untrained) if not x]
@@ -61,23 +56,29 @@ if __name__ == '__main__':
     ax_colors = (ax_cmap(np.arange(len(ckpt_ids)) / (len(ckpt_ids) + 1)))
     fig = plt.figure(figsize=(11, 8))
     ax = fig.add_axes((.1, .1, .7, .4))
-    for idx,fname in enumerate(model_scores):
-        score_dat = pd.read_pickle(fname[0])
+    all_scores=[]
+    for idx, fname in enumerate(model_scores):
+        score_dat = pd.read_pickle(fname)
         score_xr = score_dat['data']
-        #layers = model_layers[args.model_name]
+        # layers = model_layers[args.model_name]
         r3 = np.arange(len(score_xr))
-        y=score_xr.values[:,0]
-        ax.plot(r3, y, linewidth=3, color=ax_colors[idx, :], label=f'{temp[idx]}, ckpnt {ckpt_ids[idx]},trained={not(untrained[idx])}', marker='.', markersize=10)
-        #ax.fill_between(r3, y - yerr, y + yerr, facecolor=all_col[1, :], alpha=0.1)
+        y = score_xr.values[:, 0]
+        all_scores.append(y)
+        ax.plot(r3, y, linewidth=3, color=ax_colors[idx, :], label=f'ckpt {ckpt_ids[idx]}', marker='.', markersize=10)
 
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.set_xticks(np.arange(score_xr.shape[0]))
     ax.set_xticklabels(score_xr['layer'].values, fontsize=6, fontweight='normal', rotation=90)
     ax.set_ylabel('score')
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    #ax.set_title(f"model= {args.model_name}\nbencmarks={args.benchmark}", fontsize=10, fontweight='bold')
-    save_fname = Path(save_dir, f'gpt2_neox_pos_learned_benchmark_{args.benchmark}_result.pdf')
+    ax.legend(loc='center left', bbox_to_anchor=(1, 1),prop={'size':6})
+    #
+    ax = fig.add_axes((.1, .55, .7, .4))
+    ax.imshow(np.asarray(all_scores),aspect='auto',origin='lower',interpolation='gaussian')
+    ax.set_yticks(np.arange(len(all_scores)),fontsize=6)
+    ax.set_yticklabels(ckpt_ids,fontsize=6)
+    # ax.set_title(f"model= {args.model_name}\nbencmarks={args.benchmark}", fontsize=10, fontweight='bold')
+    save_fname = Path(save_dir, file_pattern.replace('.pkl', '_result.pdf'))
     save_fname.parent.mkdir(parents=True, exist_ok=True)
     fig.savefig(str(save_fname), transparent=False)
     fig.show()
