8dfce40a4d495ffadf549c807c4fb0e466a39195
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-17 11:31:27 -0400

    bugfix

--- neural_nlp/models/__init__.py
+++ neural_nlp/models/__init__.py
@@ -5,6 +5,7 @@ import torch
 import copy
 from tqdm import tqdm
 import argparse
+import numpy as np
 from brainio.assemblies import merge_data_arrays
 from numpy.random.mtrand import RandomState
 
@@ -20,35 +21,43 @@ def initialize_gpt2_weights(model,mu=0,sigma=0.02):
     model_perm = copy.deepcopy(model)
     n_ctx = model_perm.config.n_ctx
     orig_states = model_perm.state_dict()
+    valid_keys=['attn.c_attn.weight','attn.c_attn.bias','attn.c_proj','ln','mlp','wte','wpe']
+    to_permute=np.sum([np.sum([valid_keys[n] in s for s in list(orig_states.keys())]) for n in range(len(valid_keys))])
+    pbar=tqdm(total=to_permute,desc=f'permuting {to_permute} weights in {len(orig_states.keys())}')
     perm_states = dict.fromkeys(orig_states.keys())
-    for key in tqdm(orig_states.keys(), desc='permuting weights'):
-        True
+    for key in orig_states.keys():
         if 'attn.c_attn.weight' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = b
+            pbar.update()
         elif 'attn.c_attn.bias' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         elif 'attn.c_proj' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         # modify layer norm
         elif 'ln' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         # modify feedforward layer
         elif 'mlp' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         elif 'wte' in key or 'wpe' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         else:
             perm_states[key] = orig_states[key]
     return perm_states
--- neural_nlp/models/gpt_neox_model/__init__.py
+++ neural_nlp/models/gpt_neox_model/__init__.py
@@ -9,6 +9,7 @@ from neural_nlp.models.gpt_neox_model.modeling_gpt_neox_learned import GPTNeoXPo
 import torch
 import copy
 from tqdm import tqdm
+import numpy as np
 
 def permute_mat(mat):
     mat_flat = mat.flatten()
@@ -21,34 +22,44 @@ def permute_mat(mat):
 def initialize_gpt_neox_weights(model,mu=0,sigma=0.02):
     model_perm = copy.deepcopy(model)
     orig_states = model_perm.state_dict()
+    valid_keys = ['attn.qkv_proj.weight', 'attn.qkv_proj.bias', 'attn.out_proj', 'ln', 'mlp.fc', 'wte', 'wpe']
+    to_permute = np.sum(
+        [np.sum([valid_keys[n] in s for s in list(orig_states.keys())]) for n in range(len(valid_keys))])
+    pbar = tqdm(total=to_permute, desc=f'permuting {to_permute} weights in {len(orig_states.keys())}')
     perm_states = dict.fromkeys(orig_states.keys())
-    for key in tqdm(orig_states.keys(), desc='permuting weights'):
+    for key in orig_states.keys():
         if 'attn.qkv_proj.weight' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = b
+            pbar.update()
         elif 'attn.qkv_proj.bias' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         elif 'attn.out_proj' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         # modify layer norm
         elif 'ln' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         # modify feedforward layer
         elif 'mlp.fc' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         elif 'wte' in key or 'wpe' in key:
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(b)
+            pbar.update()
         else:
             perm_states[key] = orig_states[key]
     return perm_states
