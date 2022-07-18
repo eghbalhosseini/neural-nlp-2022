2d5d522f7fde4a868f90c5a3b2b1c9b826eb9004
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-18 17:37:22 -0400

    undo fixing error for wikitext which say 3 output expected, but 2 given

--- neural_nlp/models/implementations.py
+++ neural_nlp/models/implementations.py
@@ -717,8 +717,8 @@ class _PytorchTransformerWrapper(BrainModel, TaskModel):
             context_ids = token_ids[context_start:token_index + 1]
             tokens_tensor = torch.tensor([context_ids])
             tokens_tensor = tokens_tensor.to('cuda' if torch.cuda.is_available() else 'cpu')
-            #overall_features, _, context_encoding = self._model(tokens_tensor)
-            overall_features, context_encoding = self._model(tokens_tensor)
+            overall_features, _, context_encoding = self._model(tokens_tensor)
+            #overall_features, context_encoding = self._model(tokens_tensor)
             if layer is None:
                 context_features = overall_features[:, -1, :]
             else:
@@ -1438,6 +1438,10 @@ for untrained in False, True:
             if configuration['prefix'] == 'gpt-neox-pos-learned':
                 config.output_hidden_states = True
                 model = model_ctr.from_pretrained(configuration['weight_file'], config=config,state_dict=state_dict)
+            elif configuration['mistral']:
+                print('initializing mistral manual\n')
+                config.output_attentions=True
+                model = model_ctr.from_pretrained(configuration['weight_file'], config=config, output_hidden_states=True, state_dict=state_dict)
             else:
                 model = model_ctr.from_pretrained(configuration['weight_file'], output_hidden_states=True,state_dict=state_dict)
 
