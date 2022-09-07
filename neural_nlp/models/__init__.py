d8667998ea20c1228c5ef121e2d735cc57266773
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-09-07 11:19:49 -0400

    adding init2 models

--- neural_nlp/models/__init__.py
+++ neural_nlp/models/__init__.py
@@ -29,45 +29,53 @@ def initialize_gpt2_weights(model,mu=0,sigma=0.02,permute=False,valid_keys=None)
         pbar = tqdm(total=to_permute, desc=f'initializing {to_permute} weights in {len(orig_states.keys())}')
     perm_states = dict.fromkeys(orig_states.keys())
     for key in orig_states.keys():
-        if 'attn.c_attn.weight' in key:
-            a = orig_states[key]
-            b = torch.normal(mu, sigma, size=a.shape)
-            perm_states[key] = permute_mat(a) if permute else permute_mat(b)
-            pbar.update()
-        elif 'attn.c_attn.bias' in key:
-            a = orig_states[key]
-            b = torch.normal(mu, sigma, size=a.shape)
-            perm_states[key] = permute_mat(a) if permute else permute_mat(b)
-            pbar.update()
-        elif 'attn.c_proj' in key:
-            a = orig_states[key]
-            b = torch.normal(mu, sigma, size=a.shape)
-            perm_states[key] = permute_mat(a) if permute else permute_mat(b)
-            pbar.update()
-        # modify layer norm
-        elif 'ln' in key:
-            a = orig_states[key]
-            b = torch.normal(mu, sigma, size=a.shape)
-            perm_states[key] = permute_mat(a) if permute else permute_mat(b)
-            pbar.update()
-        # modify feedforward layer
-        elif 'mlp' in key:
-            a = orig_states[key]
-            b = torch.normal(mu, sigma, size=a.shape)
-            perm_states[key] = permute_mat(a) if permute else permute_mat(b)
-            pbar.update()
-        elif 'wte' in key or 'wpe' in key:
-            a = orig_states[key]
-            b = torch.normal(mu, sigma, size=a.shape)
-            perm_states[key] = permute_mat(a) if permute else permute_mat(b)
-            pbar.update()
-        elif 'lm_head' in key:
+        if any([ x in key for x in valid_keys]):
             a = orig_states[key]
             b = torch.normal(mu, sigma, size=a.shape)
             perm_states[key] = permute_mat(a) if permute else permute_mat(b)
             pbar.update()
         else:
             perm_states[key] = orig_states[key]
+
+        # if 'attn.c_attn.weight' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # elif 'attn.c_attn.bias' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # elif 'attn.c_proj' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # # modify layer norm
+        # elif 'ln' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # # modify feedforward layer
+        # elif 'mlp' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # elif 'wte' in key or 'wpe' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # elif 'lm_head' in key:
+        #     a = orig_states[key]
+        #     b = torch.normal(mu, sigma, size=a.shape)
+        #     perm_states[key] = permute_mat(a) if permute else permute_mat(b)
+        #     pbar.update()
+        # else:
+        #     perm_states[key] = orig_states[key]
     return perm_states
 
 
