e5f1e587339129c3615c9d0495c4170dfc1ec574
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-04-25 15:25:50 -0400

    adding gpt-neox-pos-learned config

--- neural_nlp/models/implementations.py
+++ neural_nlp/models/implementations.py
@@ -1199,6 +1199,7 @@ for identifier, num_layers in [
 
 
 checkpoints=['/om/weka/evlab/ehoseini/MyData/miniBERTa_training/miniBERTa_1b_v2/gpt2/checkpoints_4/']
+
 for (identifier, num_layers), ckpnts in itertools.product([
     ('gpt2-neox-pos_learned-1B-v2', 12,)], np.arange(2500,320000+2500,2500)):
     identifier = f"{identifier}-ckpnt-{ckpnts}"
