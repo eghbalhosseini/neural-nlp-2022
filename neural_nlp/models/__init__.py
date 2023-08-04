b847cf6f8e5e56e72a97f0d5b01e0cf5740561e5
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-08-04 15:41:20 -0400

    bugfix

--- neural_nlp/models/__init__.py
+++ neural_nlp/models/__init__.py
@@ -8,53 +8,54 @@ import argparse
 import numpy as np
 from brainio.assemblies import merge_data_arrays
 from numpy.random.mtrand import RandomState
-# adding a test info 
-# another test 
+
+
 def permute_mat(mat):
     mat_flat = mat.flatten()
-    assert(mat_flat.ndim==1)
+    assert (mat_flat.ndim == 1)
     shuffle_idx = torch.randperm(mat_flat.shape[0])
     mat_flat_rnd = mat_flat[shuffle_idx]
     mat_perm = torch.reshape(mat_flat_rnd, mat.shape)
     return mat_perm
 
-def initialize_gpt2_weights(model,mu=0.0,sigma=0.02,permute=False,valid_keys=None):
+
+def initialize_gpt2_weights(model, mu=0.0, sigma=0.02, permute=False, valid_keys=None):
     model_perm = copy.deepcopy(model)
     orig_states = model_perm.state_dict()
     if valid_keys is None:
-        valid_keys=['attn.c_attn.weight','attn.c_attn.bias','attn.c_proj','ln_1','ln_2','mlp.c_fc','mlp.c_proj','wte','wpe','lm_head']
+        valid_keys = ['attn.c_attn.weight', 'attn.c_attn.bias', 'attn.c_proj', 'ln_1', 'ln_2', 'mlp.c_fc', 'mlp.c_proj',
+                      'wte', 'wpe', 'lm_head']
     if type(mu) is float:
         # make a dictionalry of mu and sigma for each key
-        mu_dict = dict.fromkeys(valid_keys,mu)
-        
+        mu_dict = dict.fromkeys(valid_keys, mu)
+
     elif type(mu) is dict:
         # add the missing keys to the mu_dict
         remaining_keys = [x for x in valid_keys if x not in mu.keys()]
-        mu_dict = {**mu, **dict.fromkeys(remaining_keys,0.0)}
+        mu_dict = {**mu, **dict.fromkeys(remaining_keys, 0.0)}
     else:
         raise ValueError('mu must be either float or dict')
     if type(sigma) is float:
         # make a dictionalry of mu and sigma for each key
-        sigma_dict = dict.fromkeys(valid_keys,sigma)
+        sigma_dict = dict.fromkeys(valid_keys, sigma)
     elif type(sigma) is dict:
         # add the missing keys to the mu_dict
         remaining_keys = [x for x in valid_keys if x not in sigma.keys()]
-        sigma_dict = {**sigma, **dict.fromkeys(remaining_keys,0.0)}
+        sigma_dict = {**sigma, **dict.fromkeys(remaining_keys, 0.0)}
     else:
         raise ValueError('sigma must be either float or dict')
 
-    to_permute=np.sum([np.sum([valid_keys[n] in s for s in list(orig_states.keys())]) for n in range(len(valid_keys))])
+    to_permute = np.sum(
+        [np.sum([valid_keys[n] in s for s in list(orig_states.keys())]) for n in range(len(valid_keys))])
     if permute:
-        pbar=tqdm(total=to_permute,desc=f'permuting {to_permute} weights in {len(orig_states.keys())}')
+        pbar = tqdm(total=to_permute, desc=f'permuting {to_permute} weights in {len(orig_states.keys())}')
     else:
         pbar = tqdm(total=to_permute, desc=f'initializing {to_permute} weights in {len(orig_states.keys())}')
     perm_states = dict.fromkeys(orig_states.keys())
     for key in orig_states.keys():
-        if any([ x in key for x in valid_keys]):
+        if any([x in key for x in valid_keys]):
             a = orig_states[key]
-            idx = [x in key for x in valid_keys].index(True)
-            mu_key= valid_keys[idx]
-            b = torch.normal(mu_dict[mu_key], sigma_dict[mu_key], size=a.shape)
+            b = torch.normal(mu_dict[key], sigma_dict[key], size=a.shape)
             perm_states[key] = permute_mat(a) if permute else permute_mat(b)
             pbar.update()
         else:
@@ -63,24 +64,26 @@ def initialize_gpt2_weights(model,mu=0.0,sigma=0.02,permute=False,valid_keys=Non
     return perm_states
 
 
-def initialize_layer_norm_uniform(model,valid_keys=None):
+def initialize_layer_norm_uniform(model, valid_keys=None):
     model_perm = copy.deepcopy(model)
     orig_states = model_perm.state_dict()
     if valid_keys is None:
-        valid_keys=['ln_1','ln_2']
-    to_permute=np.sum([np.sum([valid_keys[n] in s for s in list(orig_states.keys())]) for n in range(len(valid_keys))])
-    pbar=tqdm(total=to_permute,desc=f'initializing {to_permute} weights in {len(orig_states.keys())}')
+        valid_keys = ['ln_1', 'ln_2']
+    to_permute = np.sum(
+        [np.sum([valid_keys[n] in s for s in list(orig_states.keys())]) for n in range(len(valid_keys))])
+    pbar = tqdm(total=to_permute, desc=f'initializing {to_permute} weights in {len(orig_states.keys())}')
     perm_states = dict.fromkeys(orig_states.keys())
     for key in orig_states.keys():
-        if any([ x in key for x in valid_keys]):
+        if any([x in key for x in valid_keys]):
             a = orig_states[key]
-            b = torch.rand(a.shape,requires_grad=False)
+            b = torch.rand(a.shape, requires_grad=False)
             perm_states[key] = permute_mat(b)
             pbar.update()
         else:
             perm_states[key] = orig_states[key]
 
     return perm_states
+
 
 from neural_nlp.models.implementations import model_pool, load_model, model_layers
 
